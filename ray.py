02752697d8165ae0ae0e21ceb0374aa0762f3880
Matt Kelliher <matthewlkelliher@gmail.com>
2025-09-04 19:20:53 -0400

    Added geodesic equations

--- ray.py
+++ ray.py
@@ -32,10 +32,14 @@ class Ray:
 
         self.velo = velo # TODO Get velocity in polar coordinates working
 
+        self.dr = 0
+        self.dphi = 0
+
         self.r_s = self.black_hole.radius
     
     def update_cartesian(self):
-        self.pos_adjusted += self.velo
+        self.pos_adjusted = self.pos_adjusted + self.velo
+        self.velo = pygame.math.Vector2()
 
         self.r = math.hypot(self.pos_adjusted.x, self.pos_adjusted.y)
         self.phi = math.atan2(self.pos_adjusted.y, self.pos_adjusted.x)
@@ -44,6 +48,12 @@ class Ray:
             self.active = False
 
     def update_polar(self):
+        self.dr += self.r * self.dphi ** 2 - (c ** 2 * self.r_s) / (2 * self.r ** 2)
+        self.dphi += -2 * self.dr * self.dphi / self.r
+
+        self.r += self.dr
+        self.phi += self.dphi
+        
         if self.r <= self.r_s:
             self.active = False
 
@@ -53,7 +63,9 @@ class Ray:
         pos_real = self.pos_adjusted + self.black_hole.pos
         pos_real.y = self.HEIGHT - pos_real.y
         
-        if self.active:
+        print(self.pos_adjusted)
+
+        if not self.active:
             pygame.draw.circle(self.screen, self.color, pos_real, 1)
         
         # TODO Add trail function for gradiant light trail
@@ -74,7 +86,7 @@ if __name__ == "__main__":
                             pygame.math.Vector2(0, 0))
 
     myRay = Ray(screen,
-                pygame.math.Vector2(100, 300),
+                pygame.math.Vector2(100, 100),
                 pygame.math.Vector2(5, 0),
                 myBlackHole)
 
@@ -86,11 +98,12 @@ if __name__ == "__main__":
         screen.fill("black")
         
         myRay.update_cartesian()
+        myRay.update_polar()
 
         myBlackHole.render()
         myRay.render()
 
         pygame.display.flip()
-        clock.tick(60)
+        clock.tick(1)
 
     pygame.quit()
